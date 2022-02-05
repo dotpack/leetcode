36f8fa2a261fd95f6fd9ebe28eaa76bef68e1461
Ilia Ermolin <dotpack@gmail.com>
2022-02-05 15:50:24 +0300

    solve 0206

---
+++ src/0206-reverse-linked-list/index.spec.ts
@@ -0,0 +1,9 @@
+import { ListNode } from "../0000-core";
+import { reverseList } from "./index";
+
+describe("0206-reverse-linked-list", () => {
+  it.each([[[3, 4, 2]], [[1]]])("%o", (arr) => {
+    const a = ListNode.fromArray(arr);
+    expect(reverseList(a)?.toArray()).toEqual(arr.reverse());
+  });
+});
---
+++ src/0206-reverse-linked-list/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import { ListNode } from "../0000-core";
+
+export function reverseList(list: ListNode | null): ListNode | null {
+  let prev = null;
+  while (list) {
+    const next = list.next;
+    list.next = prev;
+    prev = list;
+    list = next;
+  }
+  return prev;
+}
