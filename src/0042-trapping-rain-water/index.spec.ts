ef5f10768befed8a37c0a67bcdbfa9c15d831edd
Ilia V. Ermolin <ilia.v.ermolin@ringcentral.com>
2022-09-18 10:37:39 +0400

    solve 0042

---
+++ src/0042-trapping-rain-water/index.spec.ts
@@ -0,0 +1,11 @@
+import { trap } from "./index";
+
+describe("0042-trapping-rain-water", () => {
+  const cases: [number[], number][] = [
+    [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
+    [[4, 2, 0, 3, 2, 5], 9],
+  ];
+  it.each(cases)("", (height, result) => {
+    expect(trap(height)).toEqual(result);
+  });
+});
