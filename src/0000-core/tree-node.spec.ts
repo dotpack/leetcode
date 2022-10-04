57dfe3de2c5cc67fdd90b3ed9f4261c6286f004c
Ilia V. Ermolin <ilia.v.ermolin@ringcentral.com>
2022-10-04 15:28:07 +0400

    add TreeNode

---
+++ src/0000-core/tree-node.spec.ts
@@ -0,0 +1,67 @@
+import { TreeNode } from "./tree-node";
+
+describe("TreeNode", () => {
+  /**
+   * @see https://assets.leetcode.com/uploads/2021/01/18/pathsum1.jpg
+   */
+  const arr = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1];
+
+  describe("#new", () => {
+    it("returns correct TreeNode", () => {
+      const l = new TreeNode("l");
+      const r = new TreeNode("r");
+      const i = new TreeNode("i", l, r);
+      expect(i.val).toBe("i");
+      expect(i.left).toBe(l);
+      expect(i.right).toBe(r);
+    });
+  });
+
+  describe("#fromArray", () => {
+    it("returns correct TreeNode", () => {
+      const x = TreeNode.fromArray(arr);
+      expect(x?.val).toBe(5);
+
+      expect(x?.left?.val).toBe(4);
+      expect(x?.left?.left?.val).toBe(11);
+      expect(x?.left?.left?.left?.val).toBe(7);
+      expect(x?.left?.left?.left?.left).toBe(null);
+      expect(x?.left?.left?.left?.right).toBe(null);
+      expect(x?.left?.left?.right?.val).toBe(2);
+      expect(x?.left?.left?.right?.left).toBe(null);
+      expect(x?.left?.left?.right?.right).toBe(null);
+      expect(x?.left?.right).toBe(null);
+
+      expect(x?.right?.val).toBe(8);
+      expect(x?.right?.left?.val).toBe(13);
+      expect(x?.right?.left?.left).toBe(null);
+      expect(x?.right?.left?.right).toBe(null);
+      expect(x?.right?.right?.val).toBe(4);
+      expect(x?.right?.right?.left).toBe(null);
+      expect(x?.right?.right?.right?.val).toBe(1);
+      expect(x?.right?.right?.right?.left).toBe(null);
+      expect(x?.right?.right?.right?.right).toBe(null);
+    });
+
+    it("returns null when array is empty", () => {
+      const x = TreeNode.fromArray([]);
+      expect(x).toBe(null);
+    });
+
+    it("returns null when array is strange", () => {
+      const x = TreeNode.fromArray([1, null, 1, null, null, 1, null, 1]);
+      expect(x?.toArray()).toEqual([1, null, 1]);
+    });
+
+    it("throws an RangeError when root is null", () => {
+      expect(() => TreeNode.fromArray([null])).toThrow(RangeError);
+    });
+  });
+
+  describe(".toArray", () => {
+    it("returns correct array", () => {
+      const x = TreeNode.fromArray(arr);
+      expect(x ? x.toArray() : null).toEqual(arr);
+    });
+  });
+});
---
+++ src/0000-core/tree-node.ts
@@ -0,0 +1,75 @@
+export class TreeNode<T = number> {
+  public val: T;
+  public left: TreeNode<T> | null;
+  public right: TreeNode<T> | null;
+
+  constructor(
+    val: T,
+    left: TreeNode<T> | null = null,
+    right: TreeNode<T> | null = null
+  ) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+
+  static fromArray<T>(arr: (T | null)[]): TreeNode<T> | null {
+    if (!arr.length) {
+      return null;
+    }
+
+    if (arr[0] === null) {
+      throw new RangeError(`wrong root`);
+    }
+    const root = new TreeNode(arr[0]);
+
+    const queue = [root];
+    for (let i = 1; i < arr.length; i += 2) {
+      const parent = queue.shift();
+      if (!parent) {
+        break;
+      }
+
+      const left = arr[i];
+      if (left !== null) {
+        const node = new TreeNode(left);
+        parent.left = node;
+        queue.push(node);
+      }
+
+      const right = arr[i + 1];
+      if (right !== null) {
+        const node = new TreeNode(right);
+        parent.right = node;
+        queue.push(node);
+      }
+    }
+
+    return root;
+  }
+
+  toArray(): (T | null)[] {
+    const result = [];
+
+    const queue: (TreeNode<T> | null)[] = [this];
+    while (queue.length) {
+      const node = queue.shift();
+      if (!node) {
+        result.push(null);
+        continue;
+      }
+      result.push(node.val);
+      queue.push(node.left);
+      queue.push(node.right);
+    }
+
+    for (let i = result.length - 1; i >= 0; i--) {
+      if (result[i] !== null) {
+        break;
+      }
+      result.pop();
+    }
+
+    return result;
+  }
+}
