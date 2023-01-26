9f383b5773752befcc99ca816d0a24a2c7e50655
Ilia Ermolin <dotpack@gmail.com>
2023-01-26 23:06:07 +0400

    solve 0787

---
+++ src/0787-cheapest-flights-within-k-stops/index.ts
@@ -0,0 +1,23 @@
+export function findCheapestPrice(
+  n: number,
+  flights: number[][],
+  src: number,
+  dst: number,
+  k: number
+): number {
+  let dist = new Array(n).fill(+Infinity);
+  dist[src] = 0;
+
+  for (let i = 0; i <= k; i++) {
+    const temp = dist.slice();
+    console.log(temp);
+    for (const [f, t, cost] of flights) {
+      if (Number.isFinite(dist[f])) {
+        temp[t] = Math.min(temp[t], dist[f] + cost);
+      }
+    }
+    dist = temp;
+  }
+
+  return Number.isFinite(dist[dst]) ? dist[dst] : -1;
+}
