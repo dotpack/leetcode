221332740ed521702b5d89336f17a71f710a3e9b
Ilia Ermolin <dotpack@gmail.com>
2023-02-26 11:28:24 +0400

    solve 0072-edit-distance (memo)

--- src/0072-edit-distance/index.ts
+++ src/0072-edit-distance/index.ts
@@ -1,21 +1,40 @@
 export function minDistance(word1: string, word2: string): number {
-  if (!word1 && !word2) {
-    return 0;
-  }
-  if (!word1) {
-    return word2.length;
-  }
-  if (!word2) {
-    return word1.length;
+  function calc(
+    word1: string,
+    word2: string,
+    i: number,
+    j: number,
+    memo: number[][]
+  ): number {
+    if (i === word1.length && j === word2.length) {
+      return 0;
+    }
+    if (i === word1.length) {
+      return word2.length - j;
+    }
+    if (j === word2.length) {
+      return word1.length - i;
+    }
+
+    if (memo[i] && memo[i][j] !== undefined) {
+      return memo[i][j];
+    }
+
+    let ans = 0;
+    if (word1[i] === word2[j]) {
+      ans = calc(word1, word2, i + 1, j + 1, memo);
+    } else {
+      const insertCount = 1 + calc(word1, word2, i, j + 1, memo);
+      const deleteCount = 1 + calc(word1, word2, i + 1, j, memo);
+      const replaceCount = 1 + calc(word1, word2, i + 1, j + 1, memo);
+      ans = Math.min(insertCount, deleteCount, replaceCount);
+    }
+
+    memo[i] = memo[i] || [];
+    memo[i][j] = ans;
+    return memo[i][j];
   }
 
-  const subWord1 = word1.substring(1);
-  const subWord2 = word2.substring(1);
-  if (word1[0] === word2[0]) {
-    return minDistance(subWord1, subWord2);
-  }
-  const insertCount = 1 + minDistance(word1, subWord2);
-  const deleteCount = 1 + minDistance(subWord1, word2);
-  const replaceCount = 1 + minDistance(subWord1, subWord2);
-  return Math.min(insertCount, deleteCount, replaceCount);
+  const memo: number[][] = [];
+  return calc(word1, word2, 0, 0, memo);
 }
